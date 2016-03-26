d2a4988d774fdf7059e36c499fcb8a74327ea180
Oliver Limoyo <oliver.limoyo@mail.mcgill.ca>
2016-03-26 15:31:38 -0400

    Actually add buoy broadcaster

---
+++ catkin_ws/src/state_estimation/servo/buoy_broadcaster.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/Point.h>
+
+//TODO: Change msg type to match CV published topic
+void servoCallback(const geometry_msgs::Point::ConstPtr& msg) {
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+    transform.setOrigin( tf::Vector3(0.0, msg->y, msg->z) );
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "robot", "buoy"));
+
+    }
+
+
+
+
+int main (int argc, char** argv){
+    ros::init(argc, argv, "buoy_broadcaster");
+    ros::NodeHandle node;
+    
+    //TODO: Change CVdummytopic to real CV published topic
+    ros::Subscriber CVsub = node.subscribe("CVdummytopic", 1000, servoCallback);
+
+    ros::spin();
+
+    return 0;
+}
+
