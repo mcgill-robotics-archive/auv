eb0f50cf9a74dfb4a0cec216bb6d35e17a4024c3
David Lavoie-Boutin <dlavoieb@users.noreply.github.com>
2016-06-16 23:47:53 -0400

    Add object pointer to the subscriber instanciation

--- catkin_ws/src/sonar_proc/src/scan_preprocessor.cpp
+++ catkin_ws/src/sonar_proc/src/scan_preprocessor.cpp
@@ -5,7 +5,7 @@
 ScanPreprocessor::ScanPreprocessor(ros::NodeHandle& nh) :
     nh_(nh)
 {
-    full_scan_sub_ = nh_.subscribe<sensor_msgs::PointCloud>("/full_scan", 10, &ScanPreprocessor::fullScanCallback);
+    full_scan_sub_ = nh_.subscribe<sensor_msgs::PointCloud>("/full_scan", 10, &ScanPreprocessor::fullScanCallback, this);
     filtered_scan_pub_ = nh_.advertise<sensor_msgs::PointCloud>("pcl_filtered", 1);
 
     updater_ = nh_.createTimer(ros::Duration(0.2), &ScanPreprocessor::updateCallback, this);
