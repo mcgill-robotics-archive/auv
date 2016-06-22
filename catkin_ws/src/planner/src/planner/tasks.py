5163c6f414b2fe8581de0033acadf227613541d0
Jana Pavlasek <jbpavlasek@gmail.com>
2016-06-21 20:33:27 -0400

    Clean up with errors remaining in tasks.py

--- catkin_ws/src/planner/src/planner/tasks.py
+++ catkin_ws/src/planner/src/planner/tasks.py
@@ -1,18 +1,11 @@
+#!/usr/bin/python
 import rospy
-import smach
-import actionlib
-from smach_ros import SimpleActionState
+from geometry_msgs.msg import Vector3, Twist
 
 
-
-
-
-
-
-#condition callback for idle since it is a condition state
-#TODO: setup to condition on a topic instead
-
+# TODO: setup to condition on a topic instead
 def idle_cond_cb(ud):
+    """Condition callback for idle since it is a condition state."""
     check = raw_input()
     if check == 1:
         return True
@@ -20,21 +13,29 @@ def idle_cond_cb(ud):
         return False
 
 
-#callbacks for move, parameter reading will be moved out of the goal callback for an easier time
-#(only the first and last 3 lines of that callback will remain)
-def move_goal_cb(userdata,goal):
-    uid=userdata.move_id
+def move_goal_cb(userdata, goal):
+    """Callbacks for move, parameter reading will be moved out of the goal
+    callback for an easier time (only the first and last 3 lines of that
+    callback will remain)"""
+    uid = userdata.move_id
 
-    params = rospy.getparams('~move'+'id')
-    time = RosTime(params.time[0],params.time[1])
-    velocity = Twist(Vector3(params.velocity[0],params.velocity[1],params.velocity[2]),Vector3(params.velocity[3],params.velocity[4],params.velocity[5]))
-    move_goal = MoveGoal(time,velocity,theta,depth)
+    params = rospy.getparams('~move' + 'id')
+    time = RosTime(params.time[0], params.time[1])
+    velocity = Twist(Vector3(params.velocity[0],
+                             params.velocity[1],
+                             params.velocity[2]),
+                     Vector3(params.velocity[3],
+                             params.velocity[4],
+                             params.velocity[5]))
+    move_goal = MoveGoal(time, velocity, theta, depth)
     move_goal = userdata.movelist[uid]
     userdata.move_id += 1
     return move_goal
-#TODO: make this callback less limited and allow more arbitrary IDs
-def move_result_cb(userdata,status,goal):
+
+
+# TODO: make this callback less limited and allow more arbitrary IDs
+def move_result_cb(userdata, status, goal):
     if status == GoalStatus.SUCCEEDED:
-        if userdata.move_id==rospy.getparams('~numberofmoves'):
+        if userdata.move_id == rospy.getparams('~numberofmoves'):
             userdata.move_id = 0
             return 'done'
