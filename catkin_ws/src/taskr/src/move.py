eaeb991d1f2c8f33ad4868927208a0244cdf9417
Jana Pavlasek <jbpavlasek@gmail.com>
2016-06-23 22:58:16 -0400

    Add taskr template

---
+++ catkin_ws/src/taskr/src/move.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+
+class Move(object):
+    def __init__(self, point):
+        pass
---
+++ catkin_ws/src/taskr/src/visual_servo.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+
+class VisualServo(object):
+    def __init__(self, target):
+        pass
