c3ed0981f0740073cb3cfcd50c6fe18e262d79ee
Jana Pavlasek <jbpavlasek@gmail.com>
2016-06-24 20:26:15 -0400

    Add move stuff and clean up tasks -- not tested yet

--- catkin_ws/src/taskr/src/move.py
+++ catkin_ws/src/taskr/src/move.py
@@ -1,6 +1,73 @@
 #!/usr/bin/env python
+import rospy
+from actionlib import SimpleActionClient
+from geometry_msgs.msg import Point
+from auv_msgs.msg import SetVelocityAction, SetVelocityGoal
 
 
 class Move(object):
+    """Move action."""
+
+    VELOCITY = 5  # Apparently "no units"
+
     def __init__(self, point):
-        pass
+        self.goal = Point(x=point["position"]["x"],
+                          y=point["position"]["y"],
+                          z=point["position"]["z"])
+        self.feedback = point["feedback"] if "feedback" in point else False
+        self.yaw = point["yaw"] if "yaw" in point else None
+
+        # Create velocity action client
+        self.vel_client = SimpleActionClient('controls_velocity', SetVelocityAction)
+
+    def start(self, server, feedback_msg):
+        print "Starting move goal."
+        rate = rospy.Rate(10)
+
+        ctrl_goal = SetVelocityGoal()
+        ctrl_goal.cmd.depth = self.goal.z
+        ctrl_goal.cmd.yaw = self.yaw
+
+        time = self.getTime()
+
+        # Send yaw goal without velocity first.
+        if self.yaw != 0:
+            print "Sending yaw"
+            # Send to velocity server
+            self.vel_client.send_goal(ctrl_goal)
+            # Check if we received preempt request from Taskr.
+            if server.is_preempt_requested():
+                print "Move preempted"
+                # Send preempt request to Controls
+                self.vel_client.cancel_goal()
+                self._as.set_preempted()
+                return
+
+            self.vel_client.wait_for_result()
+
+        ctrl_goal.cmd.surgeSpeed = self.VELOCITY
+
+        # Send surge commands.
+        for s in range(1, time * 10 + 1, 1):  # No idea what this is.
+            print 'Sending Surge'
+            self.vel_client.send_goal(ctrl_goal)
+            # Check if we received preempt request from Planner
+            if self.server.is_preempt_requested():
+                print "Taskr preempted"
+                # Send preempt request to Controls
+                self.vel_client.cancel_goal()
+                self._as.set_preempted()
+                return
+
+            feedback_msg = False  # Not super useful feedback.
+            server.publish_feedback(feedback_msg)
+
+            # Sleep for the amount of time that makes the for-loop run
+            # for 1 second.
+            rate.sleep()
+
+        print "Done move."
+
+    def getTime(self):
+        dist = pow(pow(self.goal.x, 2) + pow(self.goal.y, 2), 0.5)
+        return dist / self.VELOCITY
--- catkin_ws/src/taskr/src/taskr.py
+++ catkin_ws/src/taskr/src/taskr.py
@@ -8,108 +8,142 @@ from visual_servo import VisualServo
 from acoustic_servo import AcousticServo
 from rospkg import RosPack
 from actionlib import SimpleActionServer
-from actionlib import SimpleActionClient
 from planner.msg import moveFeedback, moveResult, moveAction
-from auv_msgs.msg import SetVelocityAction, SetVelocityGoal
 
 TASK_PATH = RosPack().get_path("taskr") + "/tasks/"
 
 
-class Taskr(object):
+class Task(object):
+
+    """Task object.
+
+    Base class for each task, responsible for executing the actions belonging
+    to each task as defined in a configuration file.
+    """
+
     _feedback = moveFeedback()
     _result = moveResult()
 
-    def __init__(self, name):
+    def __init__(self, name, function):
         # Create move action server
         self._action_name = name
         self._as = SimpleActionServer(
             self._action_name, moveAction,
-            execute_cb=self.execute_cb,
+            execute_cb=function,
             auto_start=False
         )
         self._as.start()
 
     def createAction(self, action, value):
+        """Creates the appropriate action object using the name of the action
+        from the YAML key.
+
+        Args:
+            action: Name of the action.
+            value: Args of the YAML, of undefined type. The action is
+                   responsible for knowing the type, and the user is
+                   responsible for providing the appropriate data in the
+                   YAML.
+        """
         if action == "move":
-            Move(value)
+            return Move(value)
         elif action == "shoot":
-            Shoot(value)
+            return Shoot(value)
         elif action == "visual_servo":
-            VisualServo(value)
+            return VisualServo(value)
         elif action == "acoustic_servo":
-            AcousticServo(value)
+            return AcousticServo(value)
         else:
             rospy.logerr("{} is not a known type.".format(action))
 
+    def action_sequence(self, data):
+        """Iterates through the action sequence defined in the task's YAML file
+        and completes each of the actions.
 
-class Bins(Taskr):
+        Args:
+            data: Dictionary data from the YAML file.
+        """
+        for (key, value) in data.iteritems():
+            action = self.createAction(key, value)
+            action.start(self.as_, self._feedback)
+            # Check whether the action has been preempted by the client.
+            if self._as.is_preempt_requested():
+                rospy.loginfo("{}: Preempted".format(self._action_name))
+                self._as.set_preempted()
+                self._as._result = False
+                return
+
+        self._as._result = True
+        self._as.set_succeeded(self._result)
+
+
+class Bins(Task):
     YAML = "bins.yaml"
 
     def __init__(self, name):
-        self._action_name = name
+        super(Bins, self).__init__(name, self.execute_cb)
         self.data = yaml.load(TASK_PATH + self.YAML)
 
     def execute_cb(self, goal):
-        for (key, value) in self.data.iteritems():
-            self.createAction(key, value)
+        self.action_sequence(self.data)
 
 
-class Buoys(Taskr):
+class Buoys(Task):
     YAML = "buoys.yaml"
 
     def __init__(self, name):
-        self._action_name = name
+        super(Buoys, self).__init__(name, self.execute_cb)
         self.data = yaml.load(TASK_PATH + self.YAML)
 
     def execute_cb(self, goal):
-        pass
+        self.action_sequence(self.data)
 
 
-class Gate(Taskr):
+class Gate(Task):
     YAML = "gate.yaml"
 
     def __init__(self, name):
-        self._action_name = name
+        super(Gate, self).__init__(name, self.execute_cb)
         self.data = yaml.load(TASK_PATH + self.YAML)
 
     def execute_cb(self, goal):
-        pass
+        self.action_sequence(self.data)
 
 
-class Maneuver(Taskr):
+class Maneuver(Task):
     YAML = "maneuver.yaml"
 
     def __init__(self, name):
-        self._action_name = name
+        super(Maneuver, self).__init__(name, self.execute_cb)
         self.data = yaml.load(TASK_PATH + self.YAML)
 
     def execute_cb(self, goal):
-        pass
+        self.action_sequence(self.data)
 
 
-class Octagon(Taskr):
+class Octagon(Task):
     YAML = "octagon.yaml"
 
     def __init__(self, name):
-        self._action_name = name
+        super(Octagon, self).__init__(name, self.execute_cb)
         self.data = yaml.load(TASK_PATH + self.YAML)
 
     def execute_cb(self, goal):
-        pass
+        self.action_sequence(self.data)
 
 
-class Torpedo(Taskr):
+class Torpedo(Task):
     YAML = "torpedo.yaml"
 
     def __init__(self, name):
-        self._action_name = name
+        super(Torpedo, self).__init__(name, self.execute_cb)
         self.data = yaml.load(TASK_PATH + self.YAML)
 
     def execute_cb(self, goal):
-        pass
+        self.action_sequence(self.data)
 
 
-class Square(Taskr):
+class Square(Task):
     """Test task."""
     YAML = "square.yaml"
 
@@ -117,68 +151,11 @@ class Square(Taskr):
     _result = moveResult()
 
     def __init__(self, name):
-        # Create move action server
+        super(Square, self).__init__(name, self.execute_cb)
         self.data = yaml.load(TASK_PATH + self.YAML)
-        self._action_name = name
-        self._as = SimpleActionServer(
-            self._action_name, moveAction,
-            execute_cb=self.execute_cb,
-            auto_start=False
-        )
-        self._as.start()
 
     def execute_cb(self, goal):
-        print 'Received goal'
-        print goal
-        # Create velocity action client
-        vel_client = SimpleActionClient('controls_velocity', SetVelocityAction)
-
-        time = int(goal.time.secs)
-        velocity = goal.velocity
-        theta = goal.theta
-        depth = goal.depth
-
-        ctrl_goal = SetVelocityGoal()
-        ctrl_goal.cmd.depth = depth
-
-        if theta != 0:
-            print 'Sending Yaw'
-            # Turn first before surging
-            ctrl_goal.cmd.yaw = theta
-            # Send to velocity server
-            vel_client.send_goal(ctrl_goal)
-            # Check if we received preempt request from Planner
-            if self._as.is_preempt_requested():
-                print "Taskr preempted"
-                # Send preempt request to Controls
-                vel_client.cancel_goal()
-                # Handle preemption
-                self._as.set_preempted()
-                return
-            vel_client.wait_for_result()
-
-        # Surge
-        rate = rospy.Rate(10)
-        for s in range(1, time * 10 + 1, 1):
-            print 'Sending Surge'
-            ctrl_goal.cmd.surgeSpeed = velocity.linear.x
-            ctrl_goal.cmd.yaw = theta
-            vel_client.send_goal(ctrl_goal)
-            # Check if we received preempt request from Planner
-            if self._as.is_preempt_requested():
-                print "Taskr preempted"
-                # Send preempt request to Controls
-                vel_client.cancel_goal()
-                # Handle preemption
-                self._as.set_preempted()
-                return
-            # Sleep for the amount of time that makes the for-loop run
-            # for 1 second.
-            rate.sleep()
-
-        self._result.success = True
-        self._as.set_succeeded(self._result)
-        print 'Success!'
+        self.action_sequence(self.data)
 
 
 if __name__ == '__main__':
