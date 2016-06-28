c1dafd60f2517cc1f99151a52f329b96c1cd2c28
Mathieu Wang <mathieu.sy.wang@gmail.com>
2016-06-28 00:02:48 -0400

    Add basic visual servo node
    Tracks distance from center of camera to tracked object
    and publish to buoy topic

--- catkin_ws/src/state_estimation/servo/buoy_broadcaster.cpp
+++ catkin_ws/src/state_estimation/servo/buoy_broadcaster.cpp
@@ -8,20 +8,18 @@
 void servoCallback(const geometry_msgs::Point::ConstPtr& msg) {
     static tf::TransformBroadcaster br;
     tf::Transform transform;
-    transform.setOrigin( tf::Vector3(0.0, msg->y, msg->z) );
+    transform.setOrigin( tf::Vector3(msg->x, msg->y, msg->z) );
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "robot", "buoy"));
 
-    }
+}
 
 
-
-
-int main (int argc, char** argv){
+int main (int argc, char** argv) {
     ros::init(argc, argv, "buoy_broadcaster");
     ros::NodeHandle node;
     
     //TODO: Change CVdummytopic to real CV published topic
-    ros::Subscriber CVsub = node.subscribe("CVdummytopic", 1000, servoCallback);
+    ros::Subscriber CVsub = node.subscribe("/buoy/red", 1000, servoCallback);
 
     ros::spin();
 
