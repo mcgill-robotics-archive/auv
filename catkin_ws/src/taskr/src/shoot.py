f1702bf2ab8d2e7194e4923c1ac487d1eae80492
Yichi Zhang <yichi.zhang@mail.mcgill.ca>
2016-06-29 00:50:56 -0400

    Added shoot.py script to shoot port torpedo; changed testing planner client

--- catkin_ws/src/taskr/src/shoot.py
+++ catkin_ws/src/taskr/src/shoot.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
+import rospy
+#import actionlib
+from auv_msgs.msg import SolenoidCommands
 
 
 class Shoot(object):
+    """ Shoot a Torpedo from the port (left side) """
     def __init__(self, torpedo):
-        pass
+       rospy.loginfo(torpedo)
+       self.torpedoName = torpedo
+       
 
     def start(self, server, feedback_msg):
-        pass
+        pub = rospy.Publisher('/electrical_interface/solenoids', SolenoidCommands, queue_size=10)
+        port_torpedo_command = SolenoidCommands()
+        port_torpedo_command.port_torpedo = True
+        rospy.loginfo(port_torpedo_command)
+        pub.publish(port_torpedo_command)
+
+        
