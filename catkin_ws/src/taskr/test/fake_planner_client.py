d14623ca19439884431db28ebfc249e614989cd3
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2016-06-29 18:59:05 -0400

    Add command line argument to choose task in fake planner

--- catkin_ws/src/taskr/test/fake_planner_client.py
+++ catkin_ws/src/taskr/test/fake_planner_client.py
@@ -1,25 +1,33 @@
 #! /usr/bin/env python
+import sys
 import rospy
 from actionlib import SimpleActionClient
 from planner.msg import TaskAction, TaskGoal
 
 
-def fake_planner_client():
-    """Fake client that sends a single goal."""
-    client = SimpleActionClient("square_task", TaskAction)
+def fake_planner_client(task_name="square"):
+    """Fake client that sends a single goal.
+
+    Keyword arguments:
+    task_name -- the task to test (default square)
+    """
+    print(task_name)
+    client = SimpleActionClient(task_name + "_task", TaskAction)
     client.wait_for_server()
 
     # Send testing task once.
-    goal = TaskGoal(task="square")
-
+    goal = TaskGoal(task=task_name)
     client.send_goal(goal)
-
     client.wait_for_result()
-
     print client.get_result()
 
 
 if __name__ == '__main__':
     rospy.init_node("fake_planner_client")
-    fake_planner_client()
+
+    # Check for command line arg to specify task.
+    if len(sys.argv) == 1:
+        fake_planner_client()
+    elif len(sys.argv) == 2:
+        fake_planner_client(sys.argv[1])
     rospy.spin()
