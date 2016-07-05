4ff8c4522673970daa0e4c7777190ff7440ab3b0
Mathieu Wang <mathieu.sy.wang@gmail.com>
2016-07-05 15:30:20 -0400

    Add sleep and fix style in Shoot action

--- catkin_ws/src/taskr/src/shoot.py
+++ catkin_ws/src/taskr/src/shoot.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python
-import rospy
 from auv_msgs.msg import SolenoidCommands
 
-#Shoot a torpedo from either port or starboard side
+import rospy
+
+
 class Shoot(object):
-    def __init__(self, torpedo):
-       rospy.loginfo("Shoot {} now!".format(torpedo))
-       self.torpedoName = torpedo
-       
+    '''Shoot a projectile'''
+    def __init__(self, projectile):
+        rospy.loginfo("Shoot {} now!".format(projectile))
+        self.projectile_name = projectile
+        self.pub = rospy.Publisher('/electrical_interface/solenoid',
+                                   SolenoidCommands, queue_size=1)
 
     def start(self, server, feedback_msg):
         feedback_msg.is_done = False
         server.publish_feedback(feedback_msg)
 
-        pub = rospy.Publisher('/electrical_interface/solenoid', SolenoidCommands, queue_size=1)
-        torpedo_command = SolenoidCommands()
+        solenoid_command = SolenoidCommands()
 
-        # Set the attribute of torpedo_command specified in the yaml
-        # file (port_torpedo or starboard_torpedo) to True
-        setattr(torpedo_command, self.torpedoName, True)
+        # Set the attribute of solenoid_command specified in the yaml
+        # file to True
+        setattr(solenoid_command, self.projectile_name, True)
 
-        # Section for preempted for good measure.
+        # Handle actionlib server preemption
         if server.is_preempt_requested():
             rospy.loginfo("Shoot preempted")
             server.set_preempted()
             return
 
-        while pub.get_num_connections() == 0:
-            rospy.loginfo("Waiting for subscribers in Shoot")
+        while self.pub.get_num_connections() == 0:
+            rospy.logdebug("Waiting for subscribers in Shoot")
             continue
 
-        pub.publish(torpedo_command)
+        self.pub.publish(solenoid_command)
+
+        # Sleep for allowing teensy and pneumatics to react
+        rospy.Rate(100).sleep()
 
         feedback_msg.is_done = True
         server.publish_feedback(feedback_msg)
-
