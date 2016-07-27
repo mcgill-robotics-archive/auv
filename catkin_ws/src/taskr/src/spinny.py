3454eb909d2afc09fa637a54216a09cb356bb1da
Jana Pavlasek <jbpavlasek@gmail.com>
2016-07-26 22:40:48 -0700

    Add spinny things

--- catkin_ws/src/taskr/src/spinny.py
+++ catkin_ws/src/taskr/src/spinny.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 import rospy
-import numpy
-from math import fabs, pi
+from math import fabs
 from actionlib import SimpleActionClient
-from geometry_msgs.msg import Point, Vector3Stamped
+from geometry_msgs.msg import Vector3Stamped
 from auv_msgs.msg import SetVelocityAction, SetVelocityGoal
 from std_msgs.msg import Float64
 
 
-class Move(object):
-    """Move action."""
+class Spinny(object):
+    """Spin action."""
 
     RATE = rospy.get_param("taskr/vel_cmd_rate", default=10)
     ANGLE_INCREMENT = 0.1
@@ -19,18 +18,12 @@ class Move(object):
         self.curr_yaw = 0
         self.curr_depth = 0
 
-        self.depth_sub = rospy.Subscriber('state_estimation/depth', Float64, self.depth_callback)
-        self.pose_sub = rospy.Subscriber('robot_state', Vector3Stamped, self.pose_callback)
-
-        self.depth = point["depth"]
-        self.yaw = point["yaw"]
-        self.sway = point["sway"] if "sway" in point else False
-        self.maintain_yaw = (self.yaw == "same")
-        self.maintain_depth = (self.depth == "same")
+        self.depth_sub = rospy.Subscriber("state_estimation/depth", Float64, self.depth_callback)
+        self.pose_sub = rospy.Subscriber("robot_state", Vector3Stamped, self.pose_callback)
 
         # Create velocity action client for controls server.
         self.vel_client = SimpleActionClient("controls_velocity", SetVelocityAction)
-        self.vel_client.wait_for_server()        
+        self.vel_client.wait_for_server()
 
     def start(self, server, feedback_msg):
         """Do the spin action.
@@ -43,19 +36,29 @@ class Move(object):
 
         ctrl_goal = SetVelocityGoal()
 
-        if self.maintain_depth:
-            ctrl_goal.cmd.depth = self.curr_depth
-        else:
-            ctrl_goal.cmd.depth = self.depth
+        # If depth was not set, take the current depth.
+        while not self.curr_depth:
+            pass
 
+        ctrl_goal.cmd.depth = self.curr_depth
 
-        for yaw in range(self.curr_yaw, self.curr_yaw + 2*pi, ANGLE_INCREMENT):
-            ctrl_goal.cmd.yaw = yaw
+        # If yaw was not set, take the current yaw.
+        while not self.curr_yaw:
+            pass
+
+        start_yaw = self.curr_yaw
+
+        ctrl_goal.cmd.yaw = start_yaw + self.ANGLE_INCREMENT
+        self.vel_client.send_goal(ctrl_goal)
+        self.vel_client.wait_for_result()
+
+        while not fabs(self.curr_yaw - start_yaw) < 0.001:
+            ctrl_goal.cmd.yaw += self.ANGLE_INCREMENT
             self.vel_client.send_goal(ctrl_goal)
 
             # Check if we received preempt request from Planner
             if server.is_preempt_requested():
-                rospy.logerr("Taskr preempted")
+                rospy.logerr("Spinny preempted")
                 # Send preempt request to Controls
                 self.vel_client.cancel_goal()
                 server.set_preempted()
@@ -71,7 +74,7 @@ class Move(object):
 
         start = rospy.Time.now()
 
-        rospy.loginfo("Done move in time {}".format((rospy.Time.now() - start).to_sec()))
+        rospy.loginfo("Done spiny in time {}".format((rospy.Time.now() - start).to_sec()))
 
     def depth_callback(self, msg):
         self.curr_depth = msg.data
