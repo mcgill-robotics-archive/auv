17ae5f56938193f20773ce9e88d2ae57d03a1463
Justin Bell <justin.bell@mail.mcgill.ca>
2016-11-03 05:26:43 +0000

    controls provides a way to interface with PID controllers for each Degree of Freedom

---
+++ catkin_ws/src/controls/src/controls.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Wrench, Vector3
+from PID import PID
+
+
+class Controller:
+    def __init__(self):
+        self.thrust_pub = rospy.Publisher('controls/wrench', Wrench)
+
+        # TODO: Add gains from rosparams like in scripts/controls
+        self.surge_pid = PID()
+        self.sway_pid = PID()
+        self.heave_pid = PID()
+
+        self.roll_pid = PID()
+        self.pitch_pid = PID()
+        self.yaw_pid = PID()
+
+    def update(self, data):
+        # TODO: check if this coordinate system is consistent
+        # with the rest of the robot
+        force = Vector3()
+        force.x = self.surge_pid.update(data.force.x)
+        force.y = self.sway_pid.update(data.force.y)
+        force.z = self.heave_pid.update(data.force.z)
+
+        torque = Vector3()
+        torque.x = self.roll_pid.update(data.torque.x)
+        torque.y = self.pitch_pid.update(data.torque.y)
+        torque.z = self.yaw_pid.update(data.torque.z)
+
+        self.thrust_pub.publish(Wrench(force, torque))
+
+    def set_point(self, data):
+        force = Vector3()
+        force.x = self.surge_pid.setPoint(data.force.x)
+        force.y = self.sway_pid.setPoint(data.force.y)
+        force.z = self.heave_pid.setPoint(data.force.z)
+
+        torque = Vector3()
+        torque.x = self.roll_pid.setPoint(data.torque.x)
+        torque.y = self.pitch_pid.setPoint(data.torque.y)
+        torque.z = self.yaw_pid.setPoint(data.torque.z)
+
+
+if __name__ == '__main__':
+    rospy.init_node('controls')
+    controller = Controller()
+    update_sub = rospy.Subscriber(
+            'controls/update', Wrench, controller.update)
+    setpoint_sub = rospy.Subscriber(
+            'controls/set_point', Wrench, controller.setpoint)
+    rospy.spin()
