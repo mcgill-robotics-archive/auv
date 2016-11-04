d34ee9158b5c72c2f3ecf73017fef854deba0077
Jana Pavlasek <jbpavlasek@gmail.com>
2016-11-04 16:25:38 -0400

    Add update git repo and submodules

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 # Setup McGill Robotics AUV workspace
 
+# Update Git repo
+echo "Pulling latest chages..."
+git pull && git submodule update --init --recursive
+echo
+
 # ROS package dependencies
 echo "Installing ROS package dependencies..."
 rosdep update
