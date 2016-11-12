2fec41e5966578b34024324c850f90a2546bdabc
Abhinav Bhandari <abhinav.bhandari@mail.mcgill.ca>
2016-11-12 17:19:23 -0500

    Added ros_paramaters for PID gains

--- catkin_ws/src/controls/src/controls.py
+++ catkin_ws/src/controls/src/controls.py
@@ -6,16 +6,35 @@ from PID import PID
 
 class Controller:
     def __init__(self):
-        self.thrust_pub = rospy.Publisher('controls/wrench', Wrench, queue_size=10)
+        self.thrust_pub = rospy.Publisher(
+                'controls/wrench', Wrench, queue_size=10)
 
         # TODO: Add gains from rosparams like in scripts/controls
-        self.surge_pid = PID()
-        self.sway_pid = PID()
-        self.heave_pid = PID()
+        self.surge_pid = PID(
+                rospy.get_param("~kp_yPos"),
+                rospy.get_param("~ki_yPos"),
+                rospy.get_param("~kd_yPos"))
+        self.sway_pid = PID(
+                rospy.get_param("~kp_xPos"),
+                rospy.get_param("~ki_xPos"),
+                rospy.get_param("~kd_xPos"))
+        self.heave_pid = PID(
+                rospy.get_param("~kp_depth"),
+                rospy.get_param("~ki_depth"),
+                rospy.get_param("~kd_depth"))
 
-        self.roll_pid = PID()
-        self.pitch_pid = PID()
-        self.yaw_pid = PID()
+        self.roll_pid = PID(
+                rospy.get_param("~kp_roll"),
+                rospy.get_param("~ki_roll"),
+                rospy.get_param("~kd_roll"))
+        self.pitch_pid = PID(
+                rospy.get_param("~kp_pitch"),
+                rospy.get_param("~ki_pitch"),
+                rospy.get_param("~kd_pitch"))
+        self.yaw_pid = PID(
+                rospy.get_param("~kp_yaw"),
+                rospy.get_param("~ki_yaw"),
+                rospy.get_param("~kd_yaw"))
 
     def update(self, data):
         force = Vector3()
