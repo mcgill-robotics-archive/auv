5715c2c7842c5cb93a382f21834f8cc3faf291da
Justin Bell <justin.bell@mail.mcgill.ca>
2016-11-13 02:51:54 +0000

    Adds a launch file for testing the depth_maintainer. Adds explicit queue_size to publishers due to ros warnings. Adds optional rosparam for setting desired depth

--- catkin_ws/src/controls/src/maintain_depth.py
+++ catkin_ws/src/controls/src/maintain_depth.py
@@ -10,7 +10,8 @@ from tf import TransformListener
 class DepthMaintainer():
     def __init__(self, desired_depth=None):
         self.listener = TransformListener()
-        self.thrust_pub = rospy.Publisher('controls/update', Wrench)
+        self.thrust_pub = rospy.Publisher(
+            'controls/update', Wrench, queue_size=10)
         self.desired_depth = desired_depth
 
         self.set_depth(desired_depth)
@@ -24,9 +25,11 @@ class DepthMaintainer():
             except (tf.LookupException, tf.ConnectivityException,
                     tf.ExtrapolationException):
                 continue
+        raise rospy.ROSInterruptException()
 
     def set_depth(self, depth):
-        set_point_pub = rospy.Publisher('controls/set_point', Wrench)
+        set_point_pub = rospy.Publisher(
+            'controls/set_point', Wrench, queue_size=10)
         translation = Vector3(None, None, depth)
         rotation = Vector3(None, None, None)
         if depth is None:
@@ -35,7 +38,7 @@ class DepthMaintainer():
         set_wrench = Wrench(translation, rotation)
         set_point_pub.publish(set_wrench)
 
-    def update(self):
+    def update(self, _):
         estimated_depth = self.get_current_depth()
         depth_error = self.desired_depth - estimated_depth
         translation = Vector3(None, None, depth_error)
@@ -46,7 +49,12 @@ class DepthMaintainer():
 
 if __name__ == '__main__':
     rospy.init_node('maintain_depth')
-    depth_maintainer = DepthMaintainer()
+
+    desired_depth = None
+    if rospy.has_param('~desired_depth'):
+        desired_depth = rospy.get_param('~desired_depth')
+    depth_maintainer = DepthMaintainer(desired_depth)
+
     timer = rospy.Timer(rospy.Duration(0.1), depth_maintainer.update)
     rospy.on_shutdown(timer.shutdown)
     rospy.spin()
