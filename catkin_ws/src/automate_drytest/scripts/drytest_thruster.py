9541e65633ca9bdbe80ca5428169f0eb84427bea
Chelsea M-C <cmyers.colet@gmail.com>
2016-11-13 16:44:57 -0500

    Add comments to thruster dry test

--- catkin_ws/src/automate_drytest/scripts/drytest_thruster.py
+++ catkin_ws/src/automate_drytest/scripts/drytest_thruster.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python
+
+"""Thruster Dry Test.
+
+This cycles through each thruster turning it on and asking the user to
+confirm functionality.
+"""
+
 import rospy
 from rospy import Publisher
 from auv_msgs.msg import MotorCommands
 
 
 def drytest_thrusters():
+    """Cycles through each thruster and asks for user confirmation"""
     for i in range(0, len(thrusters)):
+        # sets answer back to no for the new thruster
         answer = 'n'
+        # Tests the same thruster until answer is changed to y
         while(answer.lower() == 'n'):
             print '\nTesting:', thrusters[i]
             raw_input('Press any key to continue')
 
-            for j in range(0, freq):
+            for j in range(0, freq):  # publish this command at the specified frequency
                 pub.publish(create_command(i))
                 rate.sleep()
             answer = raw_input('Is it running? y/n: ')
+            # moves on to the next thruster if the answer is yes
             if answer.lower() == 'y':
                 functional[i] = 'Y'
-            if answer.lower() == 'n':
-                answer = raw_input('Press n to test again and y to move on to the next thruster ')
+            # try again if the answer is no
+            else:
+                answer = raw_input('Press n to test again and y to move on '
+                                   'to the next thruster ')
 
 
 def create_command(thruster):
+    """This function turns on one thruster and sets the rest to 0"""
     cmd = MotorCommands()
     cmd.port_bow_heave = 300 if thruster == 0 else 0
     cmd.bow_sway = 300 if thruster == 1 else 0
@@ -36,16 +50,20 @@ def create_command(thruster):
 
 if __name__ == "__main__":
     rospy.init_node("drytest_thruster")
-    freq = 10
-    rate = rospy.Rate(freq)
+    freq = 10  # set the frequency
+    rate = rospy.Rate(freq)  # set the rate
 
     pub = Publisher("/electrical_interface/motor", MotorCommands, queue_size=5)
 
+    # Keeps track of which thrusters are functional, index matches that of thrusters
     functional = ['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N']
+    # Thruster names
     thrusters = ['Port Bow Heave', 'Bow Sway', 'Starboard Bow Heave',
                  'Starboard Surge', 'Starboard Stern Heave', 'Stern Sway',
                  'Port Stern Heave', 'Port Surge']
     drytest_thrusters()
+
+    # prints a list of functional and non-functional thrusters
     print 'Functional Thrusters are: '
     for i in range(0, len(thrusters)):
         if functional[i] == 'Y':
