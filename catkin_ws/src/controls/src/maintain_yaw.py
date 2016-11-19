4c8277c71af763b9c630d92bdcfd815f9d9dc18f
Abhinav Bhandari <abhinav.bhandari@mail.mcgill.ca>
2016-11-19 15:16:06 -0500

    Added a node to maintain yaw

--- catkin_ws/src/controls/src/maintain_yaw.py
+++ catkin_ws/src/controls/src/maintain_yaw.py
@@ -15,14 +15,32 @@ class YawMaintainer():
         self.set_yaw(desired_yaw)
 
     def set_yaw(self, yaw):
-        pass
+        set_point_pub = rospy.Publisher('controls/set_point', Wrench)
+        translation = Vector3(None, None, None)
+        rotation = Vector3(None, None, yaw)
+        if yaw is None:
+            trans, rot = self.listener.lookupTransform(
+                    'initial_horizon', 'robot', rospy.Time())
+            rotation.z = rot[2]
+            self.desired_yaw = rotation.z
+        set_wrench = Wrench(translation, rotation)
+        set_point_pub.publish(set_wrench)
 
     def update(self):
-        pass
+        trans, rot = self.listener.lookupTransform(
+                    'initial_horizon', 'robot', rospy.Time())
+        estimated_yaw = rot[2]
+        yaw_error = self.desired_yaw - estimated_yaw
+        translation = Vector3(None, None, None)
+        rotation = Vector3(None, None, yaw_error)
+        error_wrench = Wrench(translation, rotation)
+        self.thrust_pub.publish(error_wrench)
 
 
 if __name__ == '__main__':
-    rospy.init_node('maintain_depth')
-    # TODO: figure out how to initialize the yaw desired
+    rospy.init_node('maintain_yaw')
+    # TODO: investigate actionLiv server
     yaw_maintainer = YawMaintainer(1)
+    timer = rospy.Timer(rospy.Duration(0.1), yaw_maintainer.update)
+    rospy.on_shutdown(timer.shutdown)
     rospy.spin()
