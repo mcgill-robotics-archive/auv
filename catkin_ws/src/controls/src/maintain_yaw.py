5b464f3e836005ef311b95c500fdc3fa4cba0428
Abhinav Bhandari <abhinav.bhandari@mail.mcgill.ca>
2016-11-19 16:05:02 -0500

    Fixed launch files and added the angle normalizer to yaw_maintainer

--- catkin_ws/src/controls/src/maintain_yaw.py
+++ catkin_ws/src/controls/src/maintain_yaw.py
@@ -2,45 +2,60 @@
 
 
 import rospy
+from math import pi
 from geometry_msgs.msg import Wrench, Vector3
 from tf import TransformListener
+import tf
 
 
 class YawMaintainer():
     def __init__(self, desired_yaw):
         self.listener = TransformListener()
         self.thrust_pub = rospy.Publisher('controls/update', Wrench)
-        self.desired_yaw = desired_yaw
-
-        self.set_yaw(desired_yaw)
+        self.desired_yaw = self.set_yaw(desired_yaw)
 
     def set_yaw(self, yaw):
-        set_point_pub = rospy.Publisher('controls/set_point', Wrench)
-        translation = Vector3(None, None, None)
-        rotation = Vector3(None, None, yaw)
         if yaw is None:
-            trans, rot = self.listener.lookupTransform(
-                    'initial_horizon', 'robot', rospy.Time())
-            rotation.z = rot[2]
-            self.desired_yaw = rotation.z
-        set_wrench = Wrench(translation, rotation)
-        set_point_pub.publish(set_wrench)
+            yaw = self.get_current_yaw()
+        return yaw
 
     def update(self):
-        trans, rot = self.listener.lookupTransform(
-                    'initial_horizon', 'robot', rospy.Time())
-        estimated_yaw = rot[2]
+        estimated_yaw = self.get_current_yaw()
         yaw_error = self.desired_yaw - estimated_yaw
+        yaw_error = self.normalize_angle(yaw_error)
         translation = Vector3(None, None, None)
         rotation = Vector3(None, None, yaw_error)
         error_wrench = Wrench(translation, rotation)
         self.thrust_pub.publish(error_wrench)
 
+    def normalize_angle(angle, max_angle=pi):
+        # Returns angle between -max_angle and max_angle
+        angle = angle % (2 * pi)
+        if angle > pi:
+            angle -= 2 * pi
+        return angle
+
+    def get_current_yaw(self):
+        while not rospy.is_shutdown():
+            try:
+                trans, rot = self.listener.lookupTransform(
+                    'initial_horizon', 'robot', rospy.Time())
+                return rot[2]
+            except (tf.LookupException, tf.ConnectivityException,
+                    tf.ExtrapolationException):
+                continue
+        raise rospy.ROSInterruptException()
+
 
 if __name__ == '__main__':
     rospy.init_node('maintain_yaw')
-    # TODO: investigate actionLiv server
-    yaw_maintainer = YawMaintainer(1)
+    # TODO: investigate actionLib server
+
+    desired_yaw = None
+    if rospy.has_param('~desired_yaw'):
+        desired_yaw = rospy.get_param('~desired_yaw')
+    yaw_maintainer = YawMaintainer(desired_yaw)
+
     timer = rospy.Timer(rospy.Duration(0.1), yaw_maintainer.update)
     rospy.on_shutdown(timer.shutdown)
     rospy.spin()
