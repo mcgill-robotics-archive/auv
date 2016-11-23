1974c7bd97a91d085474e75eb323fda39b49d0e9
Jeremy <jgomallette@gmail.com>
2016-11-23 10:43:53 -0500

    Made Object Oriented. Improved integrating. Bug Fixes.

--- catkin_ws/src/state_estimation/pose/src/doubleIntPose.cpp
+++ catkin_ws/src/state_estimation/pose/src/doubleIntPose.cpp
@@ -1,127 +1,79 @@
 /*
  *==============================================================================
- *Name: Double Integration Pose
- *Creator: Jeremy Mallette
- *Last Updated: 30/10/2016
- *Updated By:
+ * Name: Double Integration Pose
+ * Creator: Jeremy Mallette
+ * Last Updated: 30/10/2016
+ * Updated By:
  *
- *Notes:
- *  This is a fairly inaccurate method of calculating the position from a set
- *      of acceleration data-points, given by the IMU.
+ * Notes:
+ *      - This is a fairly inaccurate method of calculating the position from a
+ *          set of acceleration data-points, given by the IMU.
+ *      - It makes use of the trapazoid rule to provide increased accuracy and
+ *          in consideration of special cases.
+ *
+ * TODO:
+ *      - Use the Euler Method instead of the trapazoid method.
+ *      - Use timestamps instead of a fixed rate.
  *
  *==============================================================================
  */
 
+#define RATE 10         //In Hz
+#define DELTA_T 0.1     //Must be 1/RATE
+
 //Libraries etc. ---------------------------------------------------------------
+#include "doubleIntPose.h"
 #include "ros/ros.h"
 #include "geometry_msgs/Vector3.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/TwistStamped.h"
 
 //Definitions ------------------------------------------------------------------
-class acceleration {
-    public:
-        double x;
-        double y;
-        double z;
-        unsigned long t;
-} accIn, accArr[3];
-
-class velocity {
-    public:
-        double x;
-        double y;
-        double z;
-        unsigned long t;
-} velArr[2];
-
-
-class position {
-    public:
-        double x;
-        double y;
-        float z;
-} position;
+timedVector currAcc, lastAcc, currVel, lastVel, position;
 
 //ROS --------------------------------------------------------------------------
 ros::Publisher pub, pub2;
 ros::Subscriber sub;
 
-//Callback ---------------------------------------------------------------------
-void dataCallback(const geometry_msgs::Vector3ConstPtr& data) {
-    accIn.x = data->x;
-    accIn.y = data->y;
-    accIn.z = data->z;
-    accIn.t = ros::Time::now().toNSec();
-}
-
-//Update Array -----------------------------------------------------------------
-void update(int i) {
-    accArr[i].x = accIn.x * pow(10.0,-18);
-    accArr[i].y = accIn.y * pow(10.0,-18);
-    accArr[i].z = accIn.z * pow(10.0,-18);
-    accArr[i].t = accIn.t;
-}
-
-//Take First Derivative --------------------------------------------------------
-void firstIntegral() {
-    velArr[0].x = (accArr[1].x - accArr[0].x) * (accArr[1].t - accArr[0].t);
-    velArr[0].y = (accArr[1].y - accArr[0].y) * (accArr[1].t - accArr[0].t);
-    velArr[0].z = (accArr[1].z - accArr[0].z) * (accArr[1].t - accArr[0].t);
-    velArr[0].t = accArr[1].t;
-
-    velArr[1].x = (accArr[2].x - accArr[1].x) * (accArr[2].t - accArr[1].t);
-    velArr[1].y = (accArr[2].y - accArr[1].y) * (accArr[2].t - accArr[1].t);
-    velArr[1].z = (accArr[2].z - accArr[1].z) * (accArr[2].t - accArr[1].t);
-    velArr[1].t = accArr[2].t;
-}
-//Take Second Derivative -------------------------------------------------------
-void secondIntegral() {
-    position.x += ((velArr[1].x - velArr[0].x) * (velArr[1].t - velArr[0].t)) / pow(10.0, -18);
-    position.y += ((velArr[1].y - velArr[0].y) * (velArr[1].t - velArr[0].t)) / pow(10.0, -18);
-    position.z += ((velArr[1].z - velArr[0].z) * (velArr[1].t - velArr[0].t)) / pow(10.0, -18);
-}
-
 //Publish Results --------------------------------------------------------------
 void publish() {
     //Velocity
-    geometry_msgs::TwistStamped intVelocity = geometry_msgs::TwistStamped();
-    intVelocity.twist.linear.x = velArr[1].x;
-    intVelocity.twist.linear.y = velArr[1].y;
-    intVelocity.twist.linear.z = velArr[1].z;
-    pub.publish(intVelocity);
+    geometry_msgs::TwistStamped velocity = geometry_msgs::TwistStamped();
+    velocity.twist.linear.x = currVel.getX();
+    velocity.twist.linear.y = currVel.getY();
+    velocity.twist.linear.z = currVel.getZ();
+    pub.publish(velocity);
     //Position
-    geometry_msgs::PoseStamped doubleIntPose = geometry_msgs::PoseStamped();
-    doubleIntPose.pose.position.x = position.x;
-    doubleIntPose.pose.position.y = position.y;
-    doubleIntPose.pose.position.z = position.z;
-    pub2.publish(doubleIntPose);
+    geometry_msgs::PoseStamped pose = geometry_msgs::PoseStamped();
+    pose.pose.position.x = position.getX();
+    pose.pose.position.y = position.getY();
+    pose.pose.position.z = position.getZ();
+    pub2.publish(pose);
 }
 
 //Print Data -------------------------------------------------------------------
 void printData() {
-    //Raw Data
-    ROS_INFO("DEBUG: Acceleration from IMU: %lf", accIn.x);
-    ROS_INFO("DEBUG: ROS Time: %li", accIn.t);
-    ROS_INFO("DEBUG: %li", accArr[1].t);
-    ROS_INFO("DEBUG: %li", accArr[0].t);
-    ROS_INFO("DEBUG: Time difference sample: %li", (accArr[1].t - accArr[0].t));
-    //Velocity
-    ROS_INFO("Speed in x direction: %lf", velArr[1].x);
-    ROS_INFO("Speed in y direction: %lf", velArr[1].y);
-    //Position
-    ROS_INFO("Position in x direction: %lf", position.x);
-    ROS_INFO("Position in y direction: %lf\n", position.y);
+    ROS_INFO("DEBUG: Acceleration from IMU: %lf\n", currAcc.getX());
+    ROS_INFO("Speed in x direction: %lf", currVel.getX());
+    ROS_INFO("Speed in y direction: %lf", currVel.getY());
+    ROS_INFO("Position in x direction: %lf", position.getX());
+    ROS_INFO("Position in y direction: %lf", position.getY());
+    printf("--------------------------------------\n");
 }
 
-//Shift Data -------------------------------------------------------------------
-void dataShift() {
-    for(int j = 0; j < 2; j++) {
-        accArr[j].x = accArr[j+1].x;
-        accArr[j].y = accArr[j+1].y;
-        accArr[j].z = accArr[j+1].z;
-        accArr[j].t = accArr[j+1].t;
-    }
+//Callback ---------------------------------------------------------------------
+void dataCallback(const geometry_msgs::Vector3ConstPtr& data) {
+    //Update Accelerations
+    lastAcc = currAcc;
+    currAcc.setVector(data->x, data->y, data->z);
+    //Update Velocities
+    lastVel = currVel;
+    currVel.integrate(currAcc, lastAcc, DELTA_T);
+    //Update Position
+    position.integrate(currVel, lastVel, DELTA_T);
+
+    printData();
+    publish();
 }
 
 //Main -------------------------------------------------------------------------
@@ -131,34 +83,14 @@ int main (int argc, char **argv) {
     ros::init(argc, argv, "double_int_pose");
     ros::NodeHandle node;
 
-    sub = node.subscribe("state_estimation/acc", 1000, &dataCallback);
+    sub = node.subscribe("/state_estimation/acc", 100, &dataCallback);
 
     pub = node.advertise<geometry_msgs::TwistStamped>("imu/vel", 100);
     pub2 = node.advertise<geometry_msgs::PoseStamped>("imu/pose", 100);
 
-    //Fill First Data Point
-    ros::spinOnce();
-    update(0);
-
-    //Fill Second Data Point
-    ros::spinOnce();
-    update(1);
-
-    ros::Rate r(10);
+    ros::Rate r(RATE);
     while(ros::ok()) {
-        //Updating Third Datapoint
         ros::spinOnce();
-        update(2);
-
-        //Calculate Integrals
-        firstIntegral();
-        secondIntegral();
-
-        //Publish Results and shift data
-        publish();
-        printData();
-        dataShift();
-
         r.sleep();
     }
 
