cc58698ad9e7e860e6887cb31b331ca4e2ae60bc
Jeremy <jgomallette@gmail.com>
2016-11-27 13:48:39 -0500

    Fix bugs; Add Filter

--- catkin_ws/src/state_estimation/pose/src/doubleIntPose.cpp
+++ catkin_ws/src/state_estimation/pose/src/doubleIntPose.cpp
@@ -20,14 +20,19 @@
 
 //For TEST NODE
 //Note: to test, make sure to change subscriber
-//#define RATE 10
-//#define DELTA_T 0.1
+#define RATE 10
+#define DELTA_T 0.1
 
 //For IMU
-#define RATE 8.35               //In Hz
-#define DELTA_T 0.119760479     //Must be 1/RATE
+//#define RATE 8.35               //In Hz
+//#define DELTA_T 0.119760479     //Must be 1/RATE
+#define USER_WINDOW_SIZE 4        //4-5 worked best with test node
 
 //Libraries etc. ---------------------------------------------------------------
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics.hpp>
+#include <boost/accumulators/statistics/rolling_mean.hpp>
+
 #include "doubleIntPose.h"
 #include "ros/ros.h"
 #include "geometry_msgs/Vector3.h"
@@ -35,7 +40,14 @@
 #include "geometry_msgs/TwistStamped.h"
 
 //Definitions ------------------------------------------------------------------
-timedVector currAcc, lastAcc, currVel, lastVel, position;
+vector currAcc, lastAcc, currVel, lastVel, position;
+
+namespace ba = boost::accumulators;
+namespace bt = ba::tag;
+typedef ba::accumulator_set<double, ba::stats<bt::rolling_mean> > mean;
+mean acc_x(bt::rolling_window::window_size = USER_WINDOW_SIZE);
+mean acc_y(bt::rolling_window::window_size = USER_WINDOW_SIZE);
+mean acc_z(bt::rolling_window::window_size = USER_WINDOW_SIZE);
 
 //ROS --------------------------------------------------------------------------
 ros::Publisher pub, pub2;
@@ -70,11 +82,18 @@ void printData() {
 //Callback ---------------------------------------------------------------------
 void dataCallback(const geometry_msgs::Vector3ConstPtr& data) {
     //Update Accelerations
+    acc_x(data->x);
+    acc_y(data->y);
+    acc_z(data->z);
+
     lastAcc = currAcc;
-    currAcc.setVector(data->x, data->y, data->z);
+    currAcc.setVector(ba::rolling_mean(acc_x), ba::rolling_mean(acc_y), ba::rolling_mean(acc_z));
+    //currAcc.setVector(data->x, data->y, data->z);
+
     //Update Velocities
     lastVel = currVel;
     currVel.integrate(currAcc, lastAcc, DELTA_T);
+
     //Update Position
     position.integrate(currVel, lastVel, DELTA_T);
 
@@ -89,7 +108,8 @@ int main (int argc, char **argv) {
     ros::init(argc, argv, "double_int_pose");
     ros::NodeHandle node;
 
-    sub = node.subscribe("/state_estimation/acc", 100, &dataCallback);
+    sub = node.subscribe("test/acc", 100, &dataCallback);
+    //sub = node.subscribe("/state_estimation/acc", 100, &dataCallback);
 
     pub = node.advertise<geometry_msgs::TwistStamped>("imu/vel", 100);
     pub2 = node.advertise<geometry_msgs::PoseStamped>("imu/pose", 100);
--- catkin_ws/src/state_estimation/pose/src/doubleIntPose.h
+++ catkin_ws/src/state_estimation/pose/src/doubleIntPose.h
@@ -15,39 +15,37 @@ using std::abs;
 #ifndef DOUBLEINTPOSE_H
 #define DOUBLEINTPOSE_H
 
-class timedVector {
+class vector {
     private:
         double x;
         double y;
         double z;
 
     public:
-        timedVector() {
+        vector() {
              x = 0.0;
              y = 0.0;
              z = 0.0;
         }
-
-        timedVector(double xIn, double yIn, double zIn) {
+        vector(double xIn, double yIn, double zIn) {
             x = xIn;
             y = yIn;
             z = zIn;
         }
-
-        void operator = (const timedVector &V ) {
+        void operator = (const vector &V ) {
             x = V.x;
             y = V.y;
             z = V.z;
         }
 
         void setVector(float xIn, float yIn, float zIn);
-        void integrate(timedVector &curr, timedVector &last, float deltaT);
+        void integrate(vector curr, vector last, float deltaT);
         float getX();
         float getY();
         float getZ();
 };
 
-void timedVector::setVector(float xIn, float yIn, float zIn) {
+void vector::setVector(float xIn, float yIn, float zIn) {
         x = xIn;
         y = yIn;
         z = zIn;
@@ -55,7 +53,7 @@ void timedVector::setVector(float xIn, float yIn, float zIn) {
 
 float calcAbsArea(float point_1, float point_2, float time) {
     float absArea;
-    float triArea = (abs(point_1 - point_2)) / 2;
+    float triArea = abs(point_1 - point_2) / 2;
 
     if(abs(point_1) > abs(point_2) || (abs(point_1) == abs(point_2))) {
         absArea = (triArea * time) + abs(point_2 * time);
@@ -69,44 +67,44 @@ float calcAbsArea(float point_1, float point_2, float time) {
 float calcCrossArea(float point_1, float point_2, float time) {
     float crossArea;
 
-    crossArea = ((point_1 + point_2) / 2) * time;
+    crossArea = ((point_1 - point_2) / 2) * time;
 
     return crossArea;
 }
 
-void timedVector::integrate(timedVector &curr, timedVector &last, float deltaT) {
-    if((curr.x >= 0) && (last.x >= 0)) {
+void vector::integrate(vector curr, vector last, float deltaT) {
+    if((curr.x >= 0.0) && (last.x >= 0.0)) {
         x += calcAbsArea(curr.x, last.x, deltaT);
-    } else if((curr.x <= 0) && (last.x <= 0)) {
-        x -= calcAbsArea(curr.x, last.x, deltaT);
+    } else if((curr.x <= 0.0) && (last.x <= 0.0)) {
+        x -= (calcAbsArea(curr.x, last.x, deltaT));
     } else {
         x += calcCrossArea(curr.x, last.x, deltaT);
     }
 
-    if((curr.y >= 0) && (last.y >= 0)) {
-        x += calcAbsArea(curr.y, last.y, deltaT);
-    } else if((curr.y <= 0) && (last.y <= 0)) {
-        x -= calcAbsArea(curr.y, last.y, deltaT);
+    if((curr.y >= 0.0) && (last.y >= 0.0)) {
+        y += calcAbsArea(curr.y, last.y, deltaT);
+    } else if((curr.y <= 0.0) && (last.y <= 0.0)) {
+        y -= calcAbsArea(curr.y, last.y, deltaT);
     } else {
-        x += calcCrossArea(curr.y, last.y, deltaT);
+        y += calcCrossArea(curr.y, last.y, deltaT);
     }
 
-    if((curr.z >= 0) && (last.z >= 0)) {
-        x += calcAbsArea(curr.z, last.z, deltaT);
-    } else if((curr.z <= 0) && (last.z <= 0)) {
-        x -= calcAbsArea(curr.z, last.z, deltaT);
+    if((curr.z >= 0.0) && (last.z >= 0.0)) {
+        z += calcAbsArea(curr.z, last.z, deltaT);
+    } else if((curr.z <= 0.0) && (last.z <= 0.0)) {
+        z -= calcAbsArea(curr.z, last.z, deltaT);
     } else {
-        x += calcCrossArea(curr.z, last.z, deltaT);
+        z += calcCrossArea(curr.z, last.z, deltaT);
     }
 }
 
-float timedVector::getX() {
+float vector::getX() {
     return x;
 }
-float timedVector::getY() {
+float vector::getY() {
     return y;
 }
-float timedVector::getZ() {
+float vector::getZ() {
     return z;
 }
 
