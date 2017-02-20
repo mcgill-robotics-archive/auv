db3c6f789ef294f036b567ef5c8f5e69db31c211
Jana Pavlasek <jbpavlasek@gmail.com>
2017-02-19 23:24:41 -0500

    Preliminary clean up of lane detection code

--- catkin_ws/src/state_estimation/detectors/laneDetector.cpp
+++ catkin_ws/src/state_estimation/detectors/laneDetector.cpp
@@ -20,44 +20,54 @@
 #include <stdlib.h>
 #include <math.h>
 
-using namespace cv;
-using namespace std;
 namespace enc = sensor_msgs::image_encodings;
 
 
-
-float bwThresh = 0.9;
-float thetaDiffThresh = 1.5;
-float thetaLastTime = 500;
-float rho1LastTime = 0;
-float rho2LastTime = 0;
-//These 3 variables take record of the current data and performs as a feedback of the next call
-int wrongTime = 0;
-bool foundLastTime = 0;
-
-
-
-LaneDetector::LaneDetector(ros::NodeHandle& nh)
+LaneDetector::LaneDetector(ros::NodeHandle& nh) :
+  bwThresh(0.9),
+  thetaDiffThresh(1.5),
+  thetaLastTime(500),
+  rho1LastTime(0),
+  rho2LastTime(0),
+  wrongTime(0),
+  foundLastTime(0),
+  detect_(false)
 {
-  // If you have constants to declare, do that here. If they need to be tuned
-  // often, make them ROS params.
   // ROS PUB/SUB
   image_sub_ = nh.subscribe<sensor_msgs::Image>("camera_down/image_color", 10, &LaneDetector::imageCallback, this);
   lane_pub_ = nh.advertise<auv_msgs::Lane>("state_estimation/lane", 10);
+  toggle_ = nh.advertiseService("lane_detector/set_state", &LaneDetector::setStateCallback, this);
 }
 
-Mat im2bw(Mat src, double grayThresh){
+bool LaneDetector::setStateCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
+{
+  detect_ = req.data;
+  res.success = true;
 
-    Mat dst;
+  if (req.data)
+  {
+    res.message = "Lane detection turned on.";
+  }
+  else
+  {
+    res.message = "Lane detection turned off.";
+  }
 
-    threshold(src,dst,255*grayThresh,255,CV_THRESH_BINARY);
-
-    return dst;
-
+  return true;
 }
-//Implementation of the function "image to binary image"
 
-void init(){
+// Implementation of the function "image to binary image".
+Mat LaneDetector::imageToBinary(Mat src, double grayThresh){
+
+  Mat dst;
+
+  threshold(src, dst, 255*grayThresh, 255, CV_THRESH_BINARY);
+
+  return dst;
+}
+
+void LaneDetector::init()
+{
   thetaLastTime = 500;
   wrongTime = 0;
   rho1LastTime = 0;
@@ -65,316 +75,260 @@ void init(){
   foundLastTime = 0;
 }
 
-
 void LaneDetector::imageCallback(const sensor_msgs::Image::ConstPtr& msg)
 {
-  // This function will be called every time the camera receives an image. That
-  // may be too fast, in which case, skip some.
+  // Do not do any detection unless it is turned on.
+  if (!detect_)
+  {
+    return;
+  }
 
-  // Do your processing of the image here. OpenCV will have functions to change
-  // sensor_msgs/Image into an OpenCV data type.
-
-  // The current image is in the msg variable.
-
-  auv_msgs::Lane lane;
   cv_bridge::CvImageConstPtr cv_ptr;
 
   try
-      {
-          cv_ptr = cv_bridge::toCvShare(msg, enc::BGR8);
-      }
+  {
+    cv_ptr = cv_bridge::toCvShare(msg, enc::BGR8);
+  }
   catch (cv_bridge::Exception& e)
-      {
-          ROS_ERROR("cv_bridge exception: %s", e.what());
-          return;
-      }
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
 
-  Mat RGB = cv_ptr->image;                     // Converting from ROS image to openCV image
+  // Convert from ROS image to openCV image.
+  Mat RGB = cv_ptr->image;
 
-  if(! RGB.data )                              // Check for invalid input
-    {
-      printf("No data.\n");
-      return;
-    }
+  // Check for invalid input
+  if(! RGB.data )
+  {
+    ROS_ERROR("No data.\n");
+    return;
+  }
 
-    printf("\n");
-  // printf("thetaLastTime = %f\n", thetaLastTime);
-  // printf("rho1LastTime = %f\n", rho1LastTime);
-  // printf("rho2LastTime = %f\n", rho2LastTime);
-  printf("wrongTime = %d\n", wrongTime);
+  ROS_INFO("wrongTime = %d\n", wrongTime);
 
-  namedWindow("MyWindow", WINDOW_NORMAL); //create a window with the name "MyWindow"
+  // Create a window to show the image.
+  namedWindow("MyWindow", WINDOW_NORMAL);
 
-  Mat gray;cvtColor( RGB, gray, CV_RGB2GRAY ); // Converting to gray image
+  Mat gray;
 
-  Mat bw = im2bw(gray, bwThresh);                // Converting to binary image
+  // Converting to gray image
+  cvtColor(RGB, gray, CV_RGB2GRAY);
 
-  Mat bwfilt, cdst;
+  // Converting to binary image
+  Mat bw = imageToBinary(gray, bwThresh);
 
+  Mat bw_filter, cdst;
 
-  medianBlur ( bw, bwfilt, 5 );
+  medianBlur(bw, bw_filter, 5);
 
-  // median filtering
+  // Median filtering
+  Canny(bw_filter, bw, 50, 150, 3);
 
-  Canny(bwfilt, bw, 50, 150, 3);
-
-  // canny detection
+  // Show debug window.
   imshow("MyWindow", bw);
 
   waitKey(10);
 
-
-
-  //Hough transformation section
-
+  // Hough transformation.
   cvtColor(bw, cdst, CV_GRAY2BGR);
 
   vector<Vec2f> lines;
-  HoughLines(bw, lines, 1, CV_PI/180, 100, 0, 0 );
+  HoughLines(bw, lines, 1, CV_PI/180, 100, 0, 0);
 
-  // for( size_t i = 0; i < lines.size(); i++ )
-  // {
-  //   float rho = lines[i][0], theta = lines[i][1];
-  //   Point pt1, pt2;
-  //   double a = cos(theta), b = sin(theta);
-  //   double x0 = a*rho, y0 = b*rho;
-  //   pt1.x = cvRound(x0 + 1000*(-b));
-  //   pt1.y = cvRound(y0 + 1000*(a));
-  //   pt2.x = cvRound(x0 - 1000*(-b));
-  //   pt2.y = cvRound(y0 - 1000*(a));
-  //   line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
-  //
-  // }
-  // imshow("MyWindow", cdst);
-  //
-  // waitKey(10);
+  lane_pub_.publish(findLane(lines, bw));
+}
 
+auv_msgs::Lane LaneDetector::findLane(vector<Vec2f> lines, Mat bw)
+{
+  auv_msgs::Lane lane;
 
-  if(lines.size() == 0){
+  if (lines.size() == 0)
+  {
     if(wrongTime >= 3){
       init();
     }
-    return;
+    ROS_WARN("No lines were detected.");
+    return lane;
   }
 
-  struct cluster{
-    float rho;
-    float theta;
-  };
   // Constrcting a struct to cluster detected lines
 
   int len = lines.size();
-  cluster s[len];
+  Cluster s[len];
   // construct a struct array with length "len", equal to the size of "lines"
 
   s[0].rho = lines[0][0];
   s[0].theta = lines[0][1];
 
-  for(int i = 1; i < len; i++){
+  for(int i = 1; i < len; i++)
+  {
     s[i].rho = 0.0;
     s[i].theta = 0.0;
   }
-  // initializing the array
+  // Initializing the array
 
   int numCluster = 1;
   int flag = 0;
-  // this variable takes the record of total number of length
-  for(int i = 1; i < len; i++){
-      flag = 0;
-      for(int j = 0; j < numCluster; j++){
 
-          if(abs(lines[i][0] - s[j].rho) < 45 && abs(lines[i][1] - s[j].theta) * 180 / CV_PI < 2){
-              flag = -1;
-              // j=-1 means the line is added to a cluster
-              break;
-          }
-
+  for(int i = 1; i < len; i++)
+  {
+    flag = 0;
+    for(int j = 0; j < numCluster; j++)
+    {
+      if(abs(lines[i][0] - s[j].rho) < 45 && abs(lines[i][1] - s[j].theta) * 180 / CV_PI < 2){
+        flag = -1;
+        // j=-1 means the line is added to a cluster
+        break;
       }
+    }
 
-      if(flag != -1){
-          s[numCluster].rho = lines[i][0];
-          s[numCluster].theta = lines[i][1];
-          numCluster++;
-      }
-      // if the line is added as a new cluster, increase numCluster
+    if(flag != -1)
+    {
+      s[numCluster].rho = lines[i][0];
+      s[numCluster].theta = lines[i][1];
+      numCluster++;
+    }
+    // if the line is added as a new cluster, increase numCluster
   }
 
   Mat cdst1;
+
   cvtColor(bw, cdst1, CV_GRAY2BGR);
-  for(int i=0;i<numCluster;i++){
-      float rho = s[i].rho, theta = s[i].theta;
-      Point pt1, pt2;
-      double a = cos(theta), b = sin(theta);
-      double x0 = a*rho, y0 = b*rho;
-      pt1.x = cvRound(x0 + 1000*(-b));
-      pt1.y = cvRound(y0 + 1000*(a));
-      pt2.x = cvRound(x0 - 1000*(-b));
-      pt2.y = cvRound(y0 - 1000*(a));
-      line( cdst1, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
 
-      imshow("MyWindow", cdst1);
-      waitKey(10);
-      theta=theta*180/CV_PI;
+  for(int i = 0; i < numCluster; i++)
+  {
+    float rho = s[i].rho, theta = s[i].theta;
+    Point pt1, pt2;
+    double a = cos(theta), b = sin(theta);
+    double x0 = a*rho, y0 = b*rho;
+    pt1.x = cvRound(x0 + 1000*(-b));
+    pt1.y = cvRound(y0 + 1000*(a));
+    pt2.x = cvRound(x0 - 1000*(-b));
+    pt2.y = cvRound(y0 - 1000*(a));
+    line(cdst1, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
 
-      if(theta>90.0){
-        theta=theta-180.0;
-      }
+    imshow("MyWindow", cdst1);
+    waitKey(10);
+    theta = theta * 180 / CV_PI;
 
-      //printf("rho:%f\ntheta:%f\n", rho,theta);
-
+    if(theta > 90.0)
+    {
+      theta = theta - 180.0;
+    }
   }
-  bool wrongTimePlus = 0;
-  bool found = 0;
 
-  for(int i = 0; i < numCluster-1; i++){
+  bool found = false;
 
-      for(int j = i + 1; j < numCluster; j++){
-          // if(i == 0 && j == 1){
-          //   float theta1 = s[i].theta * 180 /CV_PI;
-          //   float theta2 = s[j].theta * 180 /CV_PI;
-          //   //take record of the current theta values
-          //
-          //   if(theta1 > 90.0 || theta2 > 90.0){
-          //     theta1 = theta1 - 180.0;
-          //     theta2 = theta2 - 180.0;
-          //   }
-          //   printf("Here I am.\n");
-          //   printf("thetaLastTime - theta1: %f\n", abs(thetaLastTime - theta1));
-          //   printf("rho difference with last time: %f\n", abs((s[i].rho + s[j].rho) / 2 - (rho1LastTime + rho2LastTime)/2));
-          // }
-          float rhoDiff = abs( s[i].rho - s[j].rho );
-          float thetaDiff = abs( s[i].theta - s[j].theta ) * 180 / CV_PI;
+  for(int i = 0; i < numCluster - 1; i++)
+  {
+    for(int j = i + 1; j < numCluster; j++)
+    {
+        float rhoDiff = abs( s[i].rho - s[j].rho );
+        float thetaDiff = abs( s[i].theta - s[j].theta ) * 180 / CV_PI;
 
-          //printf("thetaDiff-%d%d = %f\n",i,j,thetaDiff);
-
-          if( thetaDiff < thetaDiffThresh && rhoDiff > 50 && rhoDiff < 100){
+        if( thetaDiff < thetaDiffThresh && rhoDiff > 50 && rhoDiff < 100)
+        {
           //if the parallel pair of line is found, do the following thing:
 
-              float theta1 = s[i].theta * 180 /CV_PI;
-              float theta2 = s[j].theta * 180 /CV_PI;
-              //take record of the current theta values
+          float theta1 = s[i].theta * 180 /CV_PI;
+          float theta2 = s[j].theta * 180 /CV_PI;
+          //take record of the current theta values
 
-              if(theta1 > 90.0 || theta2 > 90.0){
-                theta1 = theta1 - 180.0;
-                theta2 = theta2 - 180.0;
-              }
-              // adjust the value of theta to proper range
-
-              // if(thetaLastTime != 500){
-              if(foundLastTime == 1){
-                  if(abs(thetaLastTime - theta1) < 10){//&& abs((s[i].rho + s[j].rho) / 2 - (rho1LastTime + rho2LastTime)/2) < 1){
-                    // if the difference between the current theta and the theta last time is with in 10
-                      printf("Not the first time.\n");
-                      printf("theta difference with last time: %f\n", abs(thetaLastTime - theta1));
-                      printf("rho difference with last time: %f\n", abs((s[i].rho + s[j].rho) / 2 - (rho1LastTime + rho2LastTime)/2));
-                      wrongTime = 0;
-                      lane.theta1 = theta1;
-                      lane.theta2 = theta2;
-                      lane.rho1 = s[i].rho;
-                      lane.rho2 = s[j].rho;
-
-                      // printf("%f\n", theta);
-                      found=1;
-                      foundLastTime = 1;
-                      printf("rhoDIff = %f\n", rhoDiff);
-                      printf("thetaDiff = %f\n", thetaDiff);
-                      thetaLastTime = theta1;
-                      rho1LastTime = s[i].rho;
-                      rho2LastTime = s[j].rho;
-
-                  }
-                  // publish data, and take record of the data published
-                  else{
-                    found = 0;
-                    // during the process of tracing, it's beneficial to avoid noise and keep sending
-                    // the correct data(angle, position parameter)
-                    // if something else happens, it could be some random error.
-                    // if so, we publish the data collected last time.
-                    // if(wrongTime <= 3){
-                    //   printf("Position 2\n");
-                    //   lane.theta1 = thetaLastTime;
-                    //   lane.theta2 = thetaLastTime;
-                    //   lane.rho1 = rho1LastTime;
-                    //   lane.rho2 = rho2LastTime;
-                    //   found = 1;
-                    //   printf("rhoDIff = %f\n", rhoDiff);
-                    //   printf("thetaDiff = %f\n", thetaDiff);
-                    //   wrongTime++;
-                    //   wrongTimePlus = 1;
-                    // }
-                    //
-                    // else{
-                    //   printf("Position 3\n");
-                    //   // after 3 wrong times ,the lane is probably
-                    //   // let found = 0, thetaLastTime = 500
-                    //   lane.theta1 = theta1;
-                    //   lane.theta2 = theta2;
-                    //   lane.rho1 = s[i].rho;
-                    //   lane.rho2 = s[j].rho;
-                    //   printf("rhoDIff = %f\n", rhoDiff);
-                    //   printf("thetaDiff = %f\n", thetaDiff);
-                    //   found = 1;
-                    //   wrongTime=0;
-                    //   thetaLastTime = 500;
-                    //}
-
-                    // It can also be that the data we obtained previously is inaccurate.
-                  }
-              }
-
-              else{// if thetaLastTime = 500
-                // if it's the first time searching for lane
-                    // if the lane found is reliable with 3 times found
-                    printf("First time found!\n");
-                    lane.theta1 = theta1;
-                    lane.theta2 = theta2;
-                    lane.rho1 = s[i].rho;
-                    lane.rho2 = s[j].rho;
-                    found = 1;
-                    foundLastTime = 1;
-                    printf("rhoDIff = %f\n", rhoDiff);
-                    printf("thetaDiff = %f\n", thetaDiff);
-                    thetaLastTime = theta1;
-                    rho1LastTime = s[i].rho;
-                    rho2LastTime = s[j].rho;
-                // publish the original data.
-              }
+          if(theta1 > 90.0 || theta2 > 90.0)
+          {
+            theta1 = theta1 - 180.0;
+            theta2 = theta2 - 180.0;
           }
-      }
-      if(found == 1){
+          // adjust the value of theta to proper range
+
+          // if(thetaLastTime != 500){
+          if(foundLastTime == 1)
+          {
+            if(abs(thetaLastTime - theta1) < 10)
+            {
+              // if the difference between the current theta and the theta last time is with in 10
+              ROS_INFO("Not the first time.\n");
+              ROS_INFO("theta difference with last time: %f\n", abs(thetaLastTime - theta1));
+              ROS_INFO("rho difference with last time: %f\n", abs((s[i].rho + s[j].rho) / 2 - (rho1LastTime + rho2LastTime)/2));
+              wrongTime = 0;
+              lane.theta1 = theta1;
+              lane.theta2 = theta2;
+              lane.rho1 = s[i].rho;
+              lane.rho2 = s[j].rho;
+
+              found = true;
+              foundLastTime = 1;
+              ROS_INFO("rhoDIff = %f\n", rhoDiff);
+              ROS_INFO("thetaDiff = %f\n", thetaDiff);
+              thetaLastTime = theta1;
+              rho1LastTime = s[i].rho;
+              rho2LastTime = s[j].rho;
+            }
+            // publish data, and take record of the data published
+            else
+            {
+              found = false;
+            }
+          }
+          else
+          {
+            // if it's the first time searching for lane
+            // if the lane found is reliable with 3 times found
+            ROS_INFO("First time found!\n");
+            lane.theta1 = theta1;
+            lane.theta2 = theta2;
+            lane.rho1 = s[i].rho;
+            lane.rho2 = s[j].rho;
+            found = true;
+            foundLastTime = 1;
+            ROS_INFO("rhoDIff = %f\n", rhoDiff);
+            ROS_INFO("thetaDiff = %f\n", thetaDiff);
+            thetaLastTime = theta1;
+            rho1LastTime = s[i].rho;
+            rho2LastTime = s[j].rho;
+            // publish the original data.
+          }
+        }
+    }
+      if(found)
+      {
         break;
       }
   }
 
-
-
-    if(found == 1){
-        lane_pub_.publish(lane);
-        printf("Lane found.\n");
-        printf("Lane message: %f %f %f %f\n", lane.theta1,lane.theta2, lane.rho1, lane.rho2);
+    if(found)
+    {
+      ROS_INFO("Lane found.\n");
+      ROS_INFO("Lane message: %f %f %f %f\n", lane.theta1,lane.theta2, lane.rho1, lane.rho2);
+      return lane;
     }
 
-    else{
-
-      if(wrongTime <= 3){
-        printf("Lane not found but there is at least 1 line detected OR it could be random error OR quitting.\n");
+    else
+    {
+      if(wrongTime <= 3)
+      {
+        ROS_INFO("Lane not found but there is at least 1 line detected OR it could be random error OR quitting.\n");
         lane.theta1 = thetaLastTime;
         lane.theta2 = thetaLastTime;
         lane.rho1 = rho1LastTime;
         lane.rho2 = rho2LastTime;
         wrongTime++;
 
-        if(foundLastTime == 1){
-          printf("Message published.\n");
-          lane_pub_.publish(lane);
-          printf("Lane message: %f %f %f %f\n", lane.theta1,lane.theta2, lane.rho1, lane.rho2);
+        if(foundLastTime == 1)
+        {
+          ROS_INFO("Message published.\n");
+          ROS_INFO("Lane message: %f %f %f %f\n", lane.theta1, lane.theta2, lane.rho1, lane.rho2);
+          return lane;
         }
       }
-      else{
+      else
+      {
         init();
       }
     }
 
+  return lane;
 
 }
 
--- catkin_ws/src/state_estimation/detectors/laneDetector.h
+++ catkin_ws/src/state_estimation/detectors/laneDetector.h
@@ -8,10 +8,14 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <auv_msgs/Lane.h>
+#include <std_srvs/SetBool.h>
 
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/image_encodings.h>
+
+using namespace cv;
+using namespace std;
 
 class LaneDetector
 {
@@ -21,6 +25,24 @@ public:
 private:
   ros::Subscriber image_sub_;
   ros::Publisher lane_pub_;
+  ros::ServiceServer toggle_;
+  ros::NodeHandle nh;
+
+  float bwThresh;
+  float thetaDiffThresh;
+  float thetaLastTime;
+  float rho1LastTime;
+  float rho2LastTime;
+  //These 3 variables take record of the current data and performs as a feedback of the next call
+  int wrongTime;
+  bool foundLastTime;
+  bool detect_;
+
+  struct Cluster
+  {
+    float rho;
+    float theta;
+  };
 
   /**
    * @brief Callback for the image topic.
@@ -28,4 +50,21 @@ private:
    * @param msg Image message.
    */
   void imageCallback(const sensor_msgs::Image::ConstPtr& msg);
+
+  bool setStateCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
+
+  /**
+   * Resets a bunch of paramaters.
+   */
+  void init();
+
+  /**
+   * Creates some matrix of some image or something.
+   * @param  src        [description]
+   * @param  grayThresh [description]
+   * @return            [description]
+   */
+  Mat imageToBinary(Mat src, double grayThresh);
+
+  auv_msgs::Lane findLane(vector<Vec2f> lines, Mat bw);
 };
