4493ac15981dacaa6fbcdbd17a2a5a445e2257bf
Jana Pavlasek <jbpavlasek@gmail.com>
2017-07-03 20:13:28 -0400

    Add fixes from the pool test and the corresponding ability to test in sim

--- catkin_ws/src/controls/src/controls/utils.py
+++ catkin_ws/src/controls/src/controls/utils.py
@@ -9,28 +9,30 @@ def get_current_depth():
     Handles the transform lookup to get the current depth
     '''
     listener = tf.TransformListener()
+    listener.waitForTransform('robot', 'floating_horizon', rospy.Time(), rospy.Duration(2.0))
+
     try:
-        listener.waitForTransform('robot', 'floating_horizon', rospy.Time(0), rospy.Duration(1))
-        trans, rot = listener.lookupTransform(
-            'robot', 'floating_horizon', rospy.Time(0))
+        now = rospy.Time.now()
+        listener.waitForTransform('robot', 'floating_horizon', now, rospy.Duration(2.0))
+        trans, rot = listener.lookupTransform('robot', 'floating_horizon', now)
         return trans[2]
-    except (tf.LookupException, tf.ConnectivityException,
-            tf.ExtrapolationException) as e:
-        rospy.logerr_throttle(30, "Couldn't get depth transform: {}".format(e))
+    except Exception as e:
+        rospy.logerr("Couldn't get depth transform: {}".format(e))
         return 0.0
 
 
 def get_current_yaw():
     listener = tf.TransformListener()
+    listener.waitForTransform('robot', 'initial_horizon', rospy.Time(), rospy.Duration(2.0))
+
     try:
-        listener.waitForTransform('robot', 'initial_horizon', rospy.Time(0), rospy.Duration(1))
-        trans, rot = listener.lookupTransform(
-            'robot', 'initial_horizon', rospy.Time(0))
+        now = rospy.Time.now()
+        listener.waitForTransform('robot', 'initial_horizon', now, rospy.Duration(2.0))
+        trans, rot = listener.lookupTransform('robot', 'initial_horizon', now)
         (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(rot)
         return yaw
-    except (tf.LookupException, tf.ConnectivityException,
-            tf.ExtrapolationException) as e:
-        rospy.logerr_throttle(30, "Couldn't get yaw transform: {}".format(e))
+    except Exception as e:
+        rospy.logerr("Couldn't get yaw transform: {}".format(e))
         return 0.0
 
 
