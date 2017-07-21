a3a8d117876f3e95f9fabfd5e4b1fa12dd135c5f
Justin Bell <justin.bell@mail.mcgill.ca>
2017-07-20 23:40:20 -0400

    Add controller for acoustic servo

---
+++ catkin_ws/src/controls/src/controls/acoustic_servo.py
@@ -0,0 +1,31 @@
+import rospy
+from geometry_msgs.msg import PoseStamped
+from tf.transformations import euler_from_quaternion
+
+from PID import PID, rot_gains
+from utils import normalize_angle
+from servo_controller import ASyncServoController
+
+
+class AcousticServo(ASyncServoController):
+    '''
+    AcousticServo provides a controller to minimize the error between the
+    robot's current heading and the heading of the pinger
+    '''
+    def __init__(self):
+        pid = PID(*rot_gains['yaw'])
+        pub = rospy.Publisher('controls/superimposer/yaw', PoseStamped,
+                              queue_size=1)
+
+        super(YawMaintainer, self).__init__(pid, pub, 'hydrophones/heading',
+                                            PoseStamped, 0)
+
+    def get_error(self, msg):
+        quaternion = (
+            msg.orientation.x,
+            msg.orientation.y,
+            msg.orientation.z,
+            msg.orientation.w)
+        (_, _, yaw) = euler_from_quaternion(quaternion)
+        normalized_yaw_error = normalize_angle(yaw)
+        return normalized_yaw_error
