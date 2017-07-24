9c9fa83109aa06ce868967919455937da40d84be
Dylan Sandfelder <dylansandfelder@gmail.com>
2017-07-23 21:54:56 -0700

    Fixed task_point and it now publishes TaskPointArray

--- catkin_ws/src/sonar_proc/src/task_point.py
+++ catkin_ws/src/sonar_proc/src/task_point.py
@@ -30,6 +30,7 @@ import rospy
 from geometry_msgs.msg import Point
 from auv_msgs.msg import TaskStatus
 from sonar_proc.msg import ClusterArray
+from auv_msgs.msg import TaskPointsArray
 
 state = None
 DIRECT_APPROACH = [TaskStatus.OCTAGON, TaskStatus.SQUARE, TaskStatus.TORPEDO]
@@ -40,8 +41,9 @@ BUOY_APPROACH = [TaskStatus.BUOYS]
 # needs adjusting to the real thing
 # this is the distance between the "manuever centroids"
 
-MANEUVER_LENGTH = rospy.get_param("~manuever_length", default=5)
-BUOY_SIZE = rospy.get_param("~buoy_size", default=2)
+MANEUVER_LENGTH = rospy.get_param("~manuever_length", default=2.4)
+BUOY_SIZE = rospy.get_param("~buoy_size", default=2.0)
+GATE_LENGTH = rospy.get_param("~gate_length", default=3.05)
 
 
 def update_state(data):
@@ -71,25 +73,35 @@ def direct_approach_point(data):
         if distance < min_distance:
             significant_cluster = cluster
             min_distance = distance
-
-    return Point(
+    taskArr = TaskPointsArray()
+    taskArr.task = Point(
         significant_cluster.centroid.x,
         significant_cluster.centroid.y,
         0
     )
+    taskArr.pole1 = Point(0, 0, 0)
+    taskArr.pole2 = Point(0, 0, 0)
+
+    return taskArr
 
 
 def gate_approach_point(data):
-    # Creates every combination of two clusters
-    cluster_combos = itertools.combinations(data.clusters, 2)
+    # Creates correct length
+    if state.task in [TaskStatus.GATE]:
+        POLE_LENGTH = GATE_LENGTH
+    elif state.task in [TaskStatus.MANEUVER]:
+        POLE_LENGTH = MANEUVER_LENGTH
+    else:
+        POLE_LENGTH = 1.0
 
     # Search for the pair of clusters whose distances are closest to the
     # distance between the gate posts
-    significant_combo = cluster_combos[0]
+    significant_combo = [data.clusters[0], data.clusters[1]]
     error = centroid_distance(significant_combo[0], significant_combo[1])
-    min_error = get_distance(error, MANEUVER_LENGTH)
-    for combo in cluster_combos:
-        error = get_distance(combo[0], combo[1])
+    min_error = get_distance(error, POLE_LENGTH)
+    for combo in itertools.combinations(data.clusters, 2):
+        temp = centroid_distance(combo[0], combo[1])
+        error = get_distance(temp, POLE_LENGTH)
         if error < min_error:
             significant_combo = combo
             min_error = error
@@ -99,11 +111,15 @@ def gate_approach_point(data):
         significant_combo[0].centroid,
         significant_combo[1].centroid
     )
-    return Point(
+    taskArr = TaskPointsArray()
+    taskArr.task = Point(
         (centroids[0].x + centroids[1].x) / 2,
         (centroids[0].y + centroids[1].y) / 2,
         0
     )
+    taskArr.pole1 = Point(centroids[0].x, centroids[0].y, 0)
+    taskArr.pole2 = Point(centroids[1].x, centroids[1].y, 0)
+    return taskArr
 
 
 def task_point(data):
@@ -112,14 +128,14 @@ def task_point(data):
 
     point = None
     if state.task in DIRECT_APPROACH:
-        point = direct_approach_point()
+        point = direct_approach_point(data)
     elif state.task in GATE_APPROACH:
-        point = gate_approach_point()
+        point = gate_approach_point(data)
     else:
         return
 
     rospy.loginfo("Sonar found task point at: {%.2f, %.2f}" %
-                  (point.x, point.y))
+                  (point.task.x, point.task.y))
     pub_goal.publish(point)
 
 
@@ -128,6 +144,5 @@ if __name__ == '__main__':
     rospy.Subscriber("sonar_proc/cluster_data", ClusterArray, task_point,
                      queue_size=10)
     rospy.Subscriber("/task", TaskStatus, update_state, queue_size=10)
-
-    pub_goal = rospy.Publisher("sonar_proc/task_point", Point, queue_size=10)
+    pub_goal = rospy.Publisher("sonar_proc/task_point", TaskPointsArray, queue_size=10)
     rospy.spin()
