10718fa164f2b523c5f1edd02adb7d931354e9a8
Jeremy <jgomallette@gmail.com>
2019-02-05 18:53:40 -0500

    Improved scaffolding for Angular project

--- catkin_ws/src/gui/frontend/src/app/app.module.ts
+++ catkin_ws/src/gui/frontend/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: '**', redirectTo:''}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FlashMessagesModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
