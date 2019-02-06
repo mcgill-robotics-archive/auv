5f02ebb84050d6fe4674d530f2c0bfd035f1828e
thefinaljob <nikolai.peram@gmail.com>
2019-02-05 20:23:32 -0500

    added body components to show data

--- catkin_ws/src/gui/frontend/src/app/components/home/home.component.ts
+++ catkin_ws/src/gui/frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  data;
 
-  ngOnInit() {
+  constructor() { 
+    this.data = {
+      voltage: 0,
+      depth: 0,
+      status: false,
+      orientation: 0
+    };
+  }
+  //@Output() dataMade = new EventEmitter();
+
+  ngOnInit(){
+    this.data = this.onClick();
+  }
+
+  ngDestroy(){
+
+  }
+
+  onClick(){
+    const data = {
+      voltage: 10,
+      depth: 25,
+      On: true,
+      orientation: 0
+    }
+    
+    return data;
   }
 
 }
