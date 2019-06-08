f2d83532aca448af55b99f70eb1d62ee4f4620e5
McGill Robotics <dev@mcgillrobotics.com>
2019-06-08 16:36:31 -0400

    Bugfix for decomposer

--- catkin_ws/src/state_estimation/scripts/decomposer.py
+++ catkin_ws/src/state_estimation/scripts/decomposer.py
@@ -9,22 +9,26 @@ from tf.transformations import euler_from_quaternion
 
 class Decomposer:
     def __init__(self):
-        rollPub = rospy.Publisher(
+        self.rollPub = rospy.Publisher(
                 '/state_estimation/roll', Float64, queue_size=1)
-        pitchPub = rospy.Publisher(
+        self.pitchPub = rospy.Publisher(
                 '/state_estimation/pitch', Float64, queue_size=1)
-        yawPub = rospy.Publisher(
+        self.yawPub = rospy.Publisher(
                 '/state_estimation/yaw', Float64, queue_size=1)
 
         self._listener = tf.TransformListener()
 
     def update_eulers(self):
         try:
+            rospy.loginfo('PIN')
             trans, rot = self._listener.lookupTransform(
                     'initial_horizon', 'robot', rospy.Time(0))
 
             (roll, pitch, yaw) = euler_from_quaternion(rot)
-            
+
+            self.rollPub.publish(roll)
+            self.pitchPub.publish(pitch)
+            self.yawPub.publish(yaw)
             return
 
         except (tf.LookupException,
@@ -32,18 +36,13 @@ class Decomposer:
                 tf.ExtrapolationException) as e:
             rospy.logdebug(e.message)
 
-    def publish(self):
-        self.rollPub.publish(roll)
-        self.pitchPub.publish(pitch)
-        self.yawPub.publish(yaw)
-
 
 if __name__ == "__main__":
     rospy.init_node('decomposer')
     decomposer = Decomposer()
-    
-    rate = rospy.Rate(100)
+
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         decomposer.update_eulers()
-        decomposer.publish()
-        rospy.spin()
+        rate.sleep()
+    rospy.spinOnce()
