cfd40b72e27f436a581ff4090e4fe77c15e1cee9
tommy clark <tjclark@ualberta.ca>
2020-02-01 16:36:45 -0500

    Mess with the EZDVL to try to make it work. The problem: not reading ANYTHING on serial after sending CS/n

--- catkin_ws/src/teledyne_navigator/scripts/EZDVL.py
+++ catkin_ws/src/teledyne_navigator/scripts/EZDVL.py
@@ -4,52 +4,78 @@ import rospy
 import time
 import bitstring as bitstring
 import serial as serial
-from Ensemble.msg import Ensemble
-from serializer import deserialize
+from teledyne_navigator.msg import Ensemble
+from teledyne_navigator import serializer
+
+def _get_ensemble(rawPacket):
+	# Read all ensemble data.
+	packet = bitstring.BitStream("0x{:02X}".format(0x7D))
+	characters = rawPacket
+	for char in characters:
+		packet.append("0x{:02X}".format(ord(char)))
+
+		return deserialize(packet, frame_id)
 
 
-class EZ_DVL(object):
-    def _get_ensemble(rawPacket):
-        # Read all ensemble data.
-        packet = bitstring.BitStream("0x{:02X}".format(0x7D))
-        characters = rawPacket
-        for char in characters:
-            packet.append("0x{:02X}".format(ord(char)))
+if __name__ == "__main__":
+	rospy.init_node("teledyne_navigator")  # Starts ROS node for this
 
-            return deserialize(packet, frame_id)
+	port = rospy.get_param("~port")  # hardware settings for DVL
+	baudrate = rospy.get_param("~baudrate")
+	frame_id = rospy.get_param("~frame")
+	timeout = rospy.get_param("~timeout")
 
-    if __name__ == "__main__":
-        rospy.init_node("teledyne_navigator")  # Starts ROS node for this
+	pub = rospy.Publisher("~ensemble", Ensemble, queue_size=1)  # settings for publishing ensemble to ROS
 
-        port = rospy.get_param("~port")  # hardware settings for DVL
-        baudrate = rospy.get_param("~baudrate")
-        frame_id = rospy.get_param("~frame")
-        timeout = rospy.get_param("~timeout")
+	conn = serial.Serial(
+	port=port, baudrate=baudrate, timeout=timeout, write_timeout=0.3)
+	#conn= serial.Serial(port= "/dev/dvl",baudrate=115200, write_timeout=0.3)
+	uninitialized = True
 
-        pub = rospy.Publisher("~ensemble", Ensemble, queue_size=1)  # settings for publishing ensemble to ROS
+	while uninitialized:
+		conn.send_break(duration = 0.3)
+		time.sleep(0.5)
+		print("Sent break")
+		reply = conn.read_all()
+		print(reply)
+		if reply == "":
+			print("No reply for DVL, waiting")
+			time.sleep(1)
+		else:
+			print("got a reply from DVL, starting data read")
+			uninitialized = False
 
-        conn = serial.Serial(
-            port=port, baudrate=baudrate, timeout=timeout)
+	running = True
 
-        uninitialized = True
+	print("Sending params")
 
-        while uninitialized:
-            conn.sendBreak(0.3)
-            conn.write("PD5/n")
-            conn.write("EX1111/n")
-            conn.write("CS/n")
-            if conn.read() == "":
-                time.sleep(0.1)
-            else:
-                uninitialized = False
+	#conn.write("PD5/n")
+	#time.sleep(0.1)
+	#conn.read_all()
+	#conn.write("EX1111/n")
+	#time.sleep(0.1)
+	#conn.read_all()
+	conn.write("CS/n")
+	#time.sleep(0.1)
+	#conn.read_all()
 
-        running = True
+	print("Starting read/publish loop")
+        #conn.flush()
+	print(running)
+	time.sleep(1)
+        print(conn.in_waiting)
+	print(conn.read_all())
 
-        while running:
-            if conn.read() == chr(0x7D):
-                rawPacket = conn.read(88)
-                Ensemble = _get_ensemble(rawPacket)
-                pub.publish(Ensemble)
-            if rospy.is_shutdown():
-                conn.close()
-                break
+	while running:
+                print(conn.in_waiting)
+	        thisByte = conn.read()
+	        print(thisByte)
+	        if thisByte == chr(0x7D):
+                    rawPacket = conn.read(87)
+	            print("raw packet is: ")
+		    print(rawPacket)
+		    Ensemble = _get_ensemble(rawPacket)
+		    pub.publish(Ensemble)
+		    if rospy.is_shutdown():
+		        conn.close()
+		        break
