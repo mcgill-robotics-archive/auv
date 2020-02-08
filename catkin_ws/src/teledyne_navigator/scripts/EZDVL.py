8b4d7239d1974c3cbb30d6a510a9e5cf8bf19b4f
McGill Robotics <dev@mcgillrobotics.com>
2020-02-08 12:47:04 -0500

    fix EZDVL.py on robot

--- catkin_ws/src/teledyne_navigator/scripts/EZDVL.py
+++ catkin_ws/src/teledyne_navigator/scripts/EZDVL.py
@@ -8,75 +8,83 @@ from teledyne_navigator.msg import Ensemble
 from teledyne_navigator.serializer import deserialize
 
 def _get_ensemble(rawPacket):
-	# Read all ensemble data.
-	packet = bitstring.BitStream("0x{:02X}".format(0x7D))
-	characters = rawPacket
-	for char in characters:
-		packet.append("0x{:02X}".format(ord(char)))
+        #Read all ensemble data.
+        packet = bitstring.BitStream("0x{:02X}".format(0x7D))
+        characters = rawPacket
+        for char in characters:
+                packet.append("0x{:02X}".format(ord(char)))
+	#print("Final length of the packet is {}".format(len(packet)))
+	test = deserialize(packet, frame_id)
 
-		return deserialize(packet, frame_id)
+	return test
 
 
 if __name__ == "__main__":
-	rospy.init_node("teledyne_navigator")  # Starts ROS node for this
+        rospy.init_node("teledyne_navigator")  # Starts ROS node for this
 
-	port = rospy.get_param("~port")  # hardware settings for DVL
-	baudrate = rospy.get_param("~baudrate")
-	frame_id = rospy.get_param("~frame")
-	timeout = rospy.get_param("~timeout")
+        port = rospy.get_param("~port")  # hardware settings for DVL
+        baudrate = rospy.get_param("~baudrate")
+        frame_id = rospy.get_param("~frame")
+        timeout = rospy.get_param("~timeout")
 
-	pub = rospy.Publisher("~ensemble", Ensemble, queue_size=1)  # settings for publishing ensemble to ROS
+        pub = rospy.Publisher("~ensemble", Ensemble, queue_size=1)  # settings for publishing ensemble to ROS
 
-	conn = serial.Serial(
-	port=port, baudrate=baudrate)
-	#conn= serial.Serial(port= "/dev/dvl",baudrate=115200, write_timeout=0.3)
-	uninitialized = True
+        conn = serial.Serial(
+        port=port, baudrate=baudrate)
+        #conn= serial.Serial(port= "/dev/dvl",baudrate=115200, write_timeout=0.3)
+        uninitialized = True
 
-	while uninitialized:
-		conn.send_break(duration = 0.3)
-		time.sleep(0.5)
-		print("Sent break")
-		reply = conn.read_all()
-		print(reply)
-		if reply == "":
-			print("No reply for DVL, waiting")
-			time.sleep(1)
-		else:
-			print("got a reply from DVL, starting data read")
-			uninitialized = False
+        while uninitialized:
+                conn.send_break(duration = 0.3)
+                time.sleep(0.5)
+                #print("Sent break")
+                reply = conn.read_all()
+                #print(reply)
+                if reply == "":
+                        print("No reply for DVL, waiting")
+                        time.sleep(1)
+                else:
+                        print("got a reply from DVL, starting data read")
+                        uninitialized = False
 
-	running = True
+        running = True
 
-	print("Sending params")
+        #print("Sending params")
 
-	conn.write("PD5\n")
-	time.sleep(0.1)
-	conn.read_all()
-	conn.write("EX1111\n")
-	time.sleep(0.1)
-	conn.read_all()
-	conn.write("CS\n")
-	time.sleep(0.1)
-	conn.read_all()
+        conn.write("PD5\n")
+        time.sleep(0.1)
+        conn.read_all()
+        conn.write("EX1111\n")
+        time.sleep(0.1)
+        conn.read_all()
+        conn.write("CS\n")
+        time.sleep(0.1)
+        conn.read_all()
 
-	print("Starting read/publish loop")
+        #print("Starting read/publish loop")
         #conn.flush()
-	print(running)
-	time.sleep(1)
-        print(conn.in_waiting)
-	print(conn.read_all())
+        #print(running)
+        time.sleep(1)
+        #print(conn.in_waiting)
+        #print(conn.read_all())
 
-	while running:
-                print(conn.in_waiting)
-               # conn.write("CS/n")
-	        thisByte = conn.read()
-	        print(thisByte)
-	        if thisByte == chr(0x7D):
+        while running:
+            if (conn.in_waiting >= 88):
+	        #print(conn.in_waiting)
+                thisByte = conn.read()
+                #print(thisByte)
+                
+                if thisByte == chr(0x7D):
                     rawPacket = conn.read(87)
-	            print("raw packet is: ")
-		    print(rawPacket)
-		    Ensemble = _get_ensemble(rawPacket)
-		    pub.publish(Ensemble)
-		    if rospy.is_shutdown():
-		        conn.close()
-		        break
+                    #print("raw packet is: ")
+                    #print(rawPacket)
+                    
+                    Ensemble = _get_ensemble(rawPacket)
+                    pub.publish(Ensemble)
+                else:
+                    conn.flush()
+                    #print('Something is wrong; Flushed the buffer!')
+                    
+	    if rospy.is_shutdown():
+                conn.close()
+                break
